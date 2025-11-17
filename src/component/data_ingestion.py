24bcfb3f6099c4c96d385511e608ccd2a592c366
ajaychitroda96011@gmail.com <mr.ajaychitroda9265@gmail.com>
2025-11-17 16:40:06 +0530

    Data transformation Done

--- src/component/data_ingestion.py
+++ src/component/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.component.data_transformation import DataTransformation
+from src.component.data_transformation import DataTransformationConfig
 
 
 # Howmany input parameter is required that we will define in DataIngestionConfig class
@@ -51,4 +53,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
