79635323da2facabf11a4c94dc201aa673a79e52
ajaychitroda96011@gmail.com <mr.ajaychitroda9265@gmail.com>
2025-11-18 15:23:04 +0530

    Model Trainer

--- src/component/data_ingestion.py
+++ src/component/data_ingestion.py
@@ -10,6 +10,10 @@ from src.component.data_transformation import DataTransformation
 from src.component.data_transformation import DataTransformationConfig
 
 
+from src.component.model_trainer import ModelTrainerConfig
+from src.component.model_trainer import ModelTrainer
+
+
 # Howmany input parameter is required that we will define in DataIngestionConfig class
 
 @dataclass
@@ -27,7 +31,7 @@ class DataIngestion:
         logging.info('Entered the data ingestion method or component')
 
         try:
-            df = pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv('notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -56,4 +60,10 @@ if __name__ == "__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modelTrainer = ModelTrainer()
+
+    score = modelTrainer.initiate_model_trainer(train_arr,test_arr)
+
+    print(score*100)
---
+++ src/component/model_trainer.py
@@ -0,0 +1,87 @@
+import os 
+import sys
+from dataclasses import dataclass
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor,
+)
+
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from src.exception import CustomException
+from src.logger import logging
+
+from src.utils import save_object
+from src.utils import evaluate_model
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path = os.path.join('artifacts','model.pkl')
+
+class ModelTrainer:
+
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+
+    
+    def initiate_model_trainer(self,train_arr,test_arr):
+        try:
+            logging.info("Spliting Training and Test Input data")
+
+            x_train,y_train,x_test,y_test = (
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
+            )
+
+            model = {
+                "Random Forest" : RandomForestRegressor(),
+                "Decision Tree" : DecisionTreeRegressor(),
+                "Gradient Boosting" : GradientBoostingRegressor(),
+                "Linear Regression" : LinearRegression(),
+                "K-Neighbors Classifier" : KNeighborsRegressor(),
+                "XGB Classifier" : XGBRegressor(),
+                "CatBoosting Classifier" : CatBoostRegressor(verbose=False),
+                "Adaboost Classifier" : AdaBoostRegressor(),
+            }
+
+            model_report:dict = evaluate_model(x_train=x_train , y_train=y_train ,x_test=x_test,y_test=y_test ,models = model)
+
+
+            # To get best model score from Dict
+            best_model_score = max(sorted(model_report.values()))
+
+            # To get best model name from dict
+
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+
+            best_model = model[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException("No best Model Found")
+            logging.info("Best Found model on both training and yesting dataset")
+
+
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+
+            predicted = best_model.predict(x_test)
+
+            r2_sc = r2_score(y_test,predicted)
+
+            return r2_sc
+
+        except Exception as e:
+            raise CustomException(e,sys)
